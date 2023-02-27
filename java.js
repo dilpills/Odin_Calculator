a0e1f1707d1bebb85d0bf09c979433f5d6ec02f8
Dylan Dao <dylanpdao@gmail.com>
2023-02-26 23:39:50 -0800

    finished calculator functions

---
+++ java.js
@@ -0,0 +1,82 @@
+let operator = "";
+let perviousValue = "";
+let currentValue = "";
+
+document.addEventListener("DOMContentLoaded", function(){
+  //store all compenents on HTML in our JS
+  let clear = document.querySelector(".clear");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  numbers.forEach((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent);
+    currentScreen.textContent = currentValue;
+  }))
+
+  operators.forEach((op) => op.addEventListener("click", function(e){
+    handleOperator(e.target.textContent)
+    previousScreen.textContent = previousValue + " " + operator;
+    currentScreen.textContent = currentValue;
+  }))
+
+  clear.addEventListener("click", function(){
+    previousValue = "";
+    currentValue = "";
+    operator = "";
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+  })
+
+  equal.addEventListener("click", function(){
+    if(currentValue != "" && previousValue != ""){
+      calculate()
+      previousScreen.textContent = "";
+      if(previousValue.Length <= 5){
+        currentScreen.textContent = previousValue;
+      } else {
+        currentScreen.textContent = previousValue.slice(0,5) + "...";
+      }
+    }
+    })
+  })
+
+function handleNumber(num){
+  if(currentValue.length <= 5){
+  currentValue += num;
+  }
+}
+
+function handleOperator(op){
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
+}
+
+function calculate(){
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
+
+  if(operator === "+"){
+    previousValue += currentValue;
+  } else if(operator === "-"){
+    previousValue -= currentValue;
+  } else if (operator === "x"){
+    previousValue *= currentValue
+  } else{
+    previousValue /= currentValue;
+  }
+
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+  currentValue = previousValue.toString();
+}
+
+function roundNumber(num){
+  return Math.round(num * 1000) / 1000;
+}
